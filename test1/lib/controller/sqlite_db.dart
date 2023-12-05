76e3131ff33d973567e8bf8ef0130fb4c5804e32
Sabrina <anissabrina136@gmail.com>
2023-12-06 00:54:49 +0800

    latest

---
+++ test1/lib/controller/sqlite_db.dart
@@ -0,0 +1,49 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class SQLiteDB {
+  static const String _dbName = "bitp3453_bmi";
+
+  Database? _db;
+
+  SQLiteDB._(); // private constuctor
+  static final SQLiteDB _instance = SQLiteDB._();
+
+  factory SQLiteDB() {
+    return _instance;
+  }
+
+  Future<Database> get database async {
+    if (_db != null) {
+      return _db!;
+    }
+    String path = join(await getDatabasesPath(), _dbName,);
+    _db = await openDatabase(path, version: 1, onCreate: (createdDb, version) async {
+      for (String tableSql in SQLiteDB.tableSQLStrings) {
+        await createdDb.execute(tableSql);
+      }
+    },);
+    return _db!;
+  }
+  static List<String> tableSQLStrings =
+  [
+    '''
+          CREATE TABLE IF NOT EXISTS bmi (
+           _Username TEXT,
+           _Weight REAL,
+           _Height REAL,
+           _Gender TEXT,
+           _Status TEXT)
+                ''',
+  ];
+
+  Future<int> insert(String tableName, Map<String, dynamic> row) async{
+    Database db = await _instance.database;
+    return await db.insert(tableName, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll(String tableName) async{
+    Database db = await _instance.database;
+    return await db.query(tableName);
+  }
+}
